f3521b462ae89cd425464e7674df01902959d595
milindvb <milind141@yahoo.com>
2018-08-20 15:24:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,43 @@ server.route({
 });
 
 server.route({
+    method: 'POST',
+    path: '/submit',
+    config: {
+
+        payload: {
+            output: 'stream',
+            parse: true,
+            allow: 'multipart/form-data'
+        },
+
+        handler: function (request, reply) {
+            var data = request.payload;
+            if (data.file) {
+                var name = data.file.hapi.filename;
+                var path = __dirname + "/uploads/" + name;
+                var file = fs.createWriteStream(path);
+
+                file.on('error', function (err) { 
+                    console.error(err) 
+                });
+
+                data.file.pipe(file);
+
+                data.file.on('end', function (err) { 
+                    var ret = {
+                        filename: data.file.hapi.filename,
+                        headers: data.file.hapi.headers
+                    }
+                    reply(JSON.stringify(ret));
+                })
+            }
+
+        }
+    }
+});
+
+server.route({
     method: 'GET',
     path: '/{name}',
     handler: (request, h) => {
@@ -26,13 +63,11 @@ server.route({
 });
 
 const init = async () => {
-
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
-
     console.log(err);
     process.exit(1);
 });
